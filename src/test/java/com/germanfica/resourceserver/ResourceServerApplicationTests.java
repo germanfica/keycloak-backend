e7c8f738039c38631611285798e4f664e6d7b7a6
German Fica <germafica@gmail.com>
2022-01-14 18:52:59 -0300

    Add Maven Project

---
+++ src/test/java/com/germanfica/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.germanfica.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
