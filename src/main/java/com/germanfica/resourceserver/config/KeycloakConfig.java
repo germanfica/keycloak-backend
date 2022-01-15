7fd712f791655b2af52508c54eb973a1f132f94c
German Fica <germafica@gmail.com>
2022-01-14 21:07:44 -0300

    Add comments to KeycloakConfig

--- src/main/java/com/germanfica/resourceserver/config/KeycloakConfig.java
+++ src/main/java/com/germanfica/resourceserver/config/KeycloakConfig.java
@@ -15,6 +15,15 @@ public class KeycloakConfig {
         return new SessionRegistryImpl();
     }
 
+    /**
+     * By Default, the Spring Security Adapter looks for a keycloak.json configuration
+     * file. You can make sure it looks at the configuration provided by the Spring
+     * Boot Adapter by adding this bean.
+     *
+     * Docs: https://www.keycloak.org/docs/latest/securing_apps/index.html#using-spring-boot-configuration
+     *
+     * @return
+     */
     @Bean
     public KeycloakConfigResolver keycloakConfigResolver(){
         return new KeycloakSpringBootConfigResolver();
